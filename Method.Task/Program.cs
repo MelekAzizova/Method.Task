d16cad3a173c3aac2e98d2273df54cf5f21b0027
MelekAzizova <147470006+MelekAzizova@users.noreply.github.com>
2023-10-17 23:18:10 +0400

    Add project files.

---
+++ Method.Task/Program.cs
@@ -0,0 +1,168 @@
+﻿namespace Method.Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region    Method yarat ad soya adi capa ver
+            //    string name = "Melek";
+            //    string surname = "Azizova";
+            //    string DadName = "Reshad";
+            //    Console.WriteLine(FullName(name));
+            //    Console.WriteLine(FullName(surname));
+            //    Console.WriteLine(FullName(name, surname, DadName));
+
+            //}
+            //static string FullName(string name)
+            //{
+            //    return name;
+            //}
+            //static string FullName(string name, string surname)
+            //{
+            //    return surname + name;
+            //}
+            //static string FullName(string name, string surname, string DadName)
+            //{
+            //    return name[0] + "." + surname + " " + DadName;
+            //
+            #endregion
+
+
+
+
+
+
+            #region Çevrənin sahəsi - S = p*r² (p=3)
+            //    int Pi = 3;
+            //    Console.WriteLine("Radiusu daxil edin: ");
+
+            //    int radius = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine(Area(Pi, radius));
+
+
+
+
+
+
+            //}
+            //static int Area(int Pi)
+            //{
+            //    return Pi;
+            //}
+            //static int Area(int Pi, int radius)
+            //{
+            //    return (Pi * radius * radius);
+            //
+            #endregion
+
+
+
+
+            #region Duzbucaqlinin sahesini tap
+            //    Console.WriteLine("Duzbucaqlinin enini daxil edin: ");
+            //    int width = Convert.ToInt32(Console.ReadLine());
+
+            //    Console.WriteLine("Duzbucaqlinin uzunlugunu qeyd edin: ");
+            //    int length = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+            //    Console.WriteLine("duzbucaqlinin uzunlugu"+"-" +Area(length));
+            //    Console.WriteLine("Duzbucaqlinin sahesi" + " " + Area(width, length));
+
+            //}
+
+
+            //static int Area(int length)
+            //{
+            //    return length;
+            //}
+            //static int Area(int length, int width)
+            //{
+            //    return length * width;
+
+            #endregion
+
+
+
+
+            #region Düzbucaqlı paralelpipedin tam səthinin sahəsi - S = 2(a * b + a * c + b * c)
+
+            //    int a = 5;
+            //    int b = 12;
+            //    int c = 16;
+            //    int d=2;
+
+            //    int area1 = Area(a, b, c);
+            //    int area2 = Area(a, b, c, d);
+            //    Console.WriteLine("Tereflerin hasili" + "-" + area1);
+            //    Console.WriteLine("Düzbucaqlı paralelpipedin tam sethinin sahes" +"-"+area2);
+
+
+
+
+
+            //}
+            //static int Area(int a, int b, int c)
+            //{
+            //    int area = (a * b + a * c + b * c);
+            //    return area;
+            //}
+
+            //static int Area(int a,int b,int c,int d)
+            //    {
+            //    return (a * b + a * c + b * c) * d;
+
+
+            #endregion
+
+
+
+            #region Üçbucaqlının daxilinə çəkilmiş çevrənin sahəsi - S=p*r; p=(a+b+c)/2
+
+            //    Console.WriteLine("Ucbucagin birinci terefin qeyd edin: ");
+            //    int FirstParty = Convert.ToInt32(Console.ReadLine());
+
+            //    Console.WriteLine("Ucbucagin ikinci terefin qeyd edin: ");
+            //    int SecondParty = Convert.ToInt32(Console.ReadLine());
+
+            //    Console.WriteLine("Ucbucagin ucuncu terefin qeyd edin: ");
+            //    int ThirdParty = Convert.ToInt32(Console.ReadLine());
+
+            //    Console.WriteLine("Cevrenin radiusunu qeyd edin: ");
+            //    int radius = Convert.ToInt32(Console.ReadLine());
+
+            //    int perimetr = Area(FirstParty, SecondParty, ThirdParty);
+
+
+            //    Console.WriteLine(Area(perimetr, radius));
+
+
+
+
+
+
+
+            //}
+            //static int Area(int FirstParty, int SecondParty, int ThirdParty)
+            //{
+            //    int perimetr = (FirstParty + SecondParty + ThirdParty) / 2;
+            //    return perimetr;
+            //}
+            //static int Area(int perimetr, int radius)
+            //{
+            //    int area = perimetr * radius;
+            //    return area;
+
+
+
+            #endregion
+
+
+
+        }
+
+    }
+    }
